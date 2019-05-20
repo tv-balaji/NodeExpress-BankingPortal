6301b0fcc21642919e2464d298a606c1c1d43b65
Balaji Venkatachalam <Balaji.Venkatachalam@digiscloud.com>
2019-05-20 13:00:37 -0500

    Completed module

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express.urlencoded({ extended: true }));
 const accountData = fs.readFileSync(
   path.join(__dirname, "json", "accounts.json"),
   "utf8"
@@ -37,6 +37,43 @@ app.get("/credit", function(request, response) {
 
 app.get("/profile", function(request, response) {
   response.render("profile", { user: users[0] });
+});
+
+app.get("/transfer", function(request, response) {
+  response.render("transfer");
+});
+
+app.post("/transfer", function(req, response) {
+  accounts[req.body.from].balance =
+    accounts[req.body.from].balance - req.body.amount;
+  accounts[req.body.to].balance =
+    parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(
+    path.join(__dirname, "json/accounts.json"),
+    accountsJSON,
+    "utf8"
+  );
+  response.render("transfer", { message: "Transfer Completed" });
+});
+
+app.get("/payment", function(request, response) {
+  response.render("payment", { account: accounts.credit });
+});
+
+app.post("/payment", function(req, response) {
+  accounts.credit.balance -= req.body.amount;
+  accounts.credit.available += parseInt(req.body.amount, 10);
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(
+    path.join(__dirname, "json", "accounts.json"),
+    accountsJSON,
+    "utf-8"
+  );
+  response.render("payment", {
+    message: "Payment Successful",
+    account: accounts.credit
+  });
 });
 
 app.listen(3000, () => {
