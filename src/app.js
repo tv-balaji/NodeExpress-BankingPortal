9d7e67622a17c010ebd2f28d023f2b4ca17d0afc
Balaji Venkatachalam <Balaji.Venkatachalam@digiscloud.com>
2019-05-20 11:50:23 -0500

    Completed module

--- src/app.js
+++ src/app.js
@@ -12,12 +12,12 @@ const accountData = fs.readFileSync(
   path.join(__dirname, "json", "accounts.json"),
   "utf8"
 );
-const accounts = JSON.parse(accountdata);
+const accounts = JSON.parse(accountData);
 const userData = fs.readFileSync(
   path.join(__dirname, "json", "users.json"),
   "utf8"
 );
-const users = JSON.parse(usersdata);
+const users = JSON.parse(userData);
 
 app.get("/", function(request, response) {
   response.render("index", { title: "Account Summary", accounts });
